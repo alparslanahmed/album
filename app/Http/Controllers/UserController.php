3455046d5675a7af79e4e1712e4acc413ab82f29
Ahmed Alparslan Özdemir <alparslanahmed@gmail.com>
2020-03-23 23:53:21 +0300

    Register Finished

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $checkEmail = User::where('email', $request->get('email'))->first();
+
+        if ($checkEmail) {
+            return response()->json(['message' => 'User with this email address exist.'], 409);
+        }
+
+        $checkUsername = User::where('username', $request->get('username'))->first();
+
+        if ($checkUsername) {
+            return response()->json(['message' => 'User with this username exist.'], 409);
+        }
+
+        User::create($request->all());
+
+        return response()->json(['message' => 'Account created, you can login now.']);
+    }
 }
