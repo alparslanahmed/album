08622e5224da86f72edce90a5d35cec19c769a6c
Ahmed Alparslan Özdemir <alparslanahmed@gmail.com>
2020-03-24 05:22:31 +0300

    Register password hash fix.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -28,13 +29,12 @@ class UserController extends Controller
             return response()->json(['message' => 'User with this username exist.'], 409);
         }
 
-        User::create($request->all());
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+
+        User::create($input);
 
         return response()->json(['message' => 'Account created, you can login now.']);
-    }
-
-    public function forgotPassword(Request $request)
-    {
-
     }
 }
